14e50541a6d0275dd7dce47b69e5c9f8c5b9846b
Atharva <atharvaj77@gmail.com>
2020-11-10 22:42:42 +0530

    Updated deprecated code

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,13 @@ import 'package:youtube_clone/extensions/account.dart';
 import 'package:youtube_clone/extensions/search.dart';
 
 void main() {
-   runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Base()
-    );
+    return MaterialApp(home: Base());
   }
 }
 
@@ -29,7 +27,7 @@ class Base extends StatefulWidget {
 
 class _BaseState extends State<Base> {
   int _currentIndex = 0;
-  final List<Widget>children =[
+  final List<Widget> children = [
     Home(),
     Explore(),
     Subscriptions(),
@@ -41,42 +39,77 @@ class _BaseState extends State<Base> {
     return MaterialApp(
       title: 'YouTube',
       home: Scaffold(
-        appBar:AppBar(
-          title: Row(children: <Widget>[
-            Row(children: <Widget>[
-            Image.asset('assets/icons/youtube.png',width: 40, height: 30,),
-            Text('Youtube',style: TextStyle(color:Colors.black,fontFamily:'BebasNeue'),
-            ),
-          ],
-          mainAxisAlignment: MainAxisAlignment.start,
-          ),
-          Spacer(),
-          Row(children: <Widget>[
-            Row(children:<Widget>[
-              GestureDetector(child: Icon(Icons.videocam, color: Colors.black,),),
-              SizedBox(width: 10),
-              GestureDetector(
-                onTap:  (){Navigator.push(context, new MaterialPageRoute(builder: (context)=> new Search()));},
-                child: Icon(Icons.search, color: Colors.black)),
-              SizedBox(width: 10),
-              GestureDetector(
-                onTap: (){Navigator.push(context, new MaterialPageRoute(builder: (context)=> new Account()));},
-                child: Image.asset('assets/icons/user.png', width: 40, height: 30,))
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Image.asset(
+                    'assets/icons/youtube.png',
+                    width: 40,
+                    height: 30,
+                  ),
+                  Text(
+                    'Youtube',
+                    style:
+                        TextStyle(color: Colors.black, fontFamily: 'BebasNeue'),
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.start,
+              ),
+              Spacer(),
+              Row(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      GestureDetector(
+                        child: Icon(
+                          Icons.videocam,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                new MaterialPageRoute(
+                                    builder: (context) => new Search()));
+                          },
+                          child: Icon(Icons.search, color: Colors.black)),
+                      SizedBox(width: 10),
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                new MaterialPageRoute(
+                                    builder: (context) => new Account()));
+                          },
+                          child: Image.asset(
+                            'assets/icons/user.png',
+                            width: 40,
+                            height: 30,
+                          ))
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.end,
+                  )
+                ],
+              )
             ],
-            mainAxisAlignment: MainAxisAlignment.end,
-            )
-          ],)
-          ],
-          ), 
-          backgroundColor: Colors.white,
           ),
-          body: children[_currentIndex],
-          bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home),title: Text('Home')),
-            BottomNavigationBarItem(icon: Icon(Icons.explore),title: Text('Explore')),
-            BottomNavigationBarItem(icon: Icon(Icons.subscriptions),title: Text('Subscriptions')),
-            BottomNavigationBarItem(icon: Icon(Icons.mail),title: Text('Inbox')),
-            BottomNavigationBarItem(icon: Icon(Icons.video_library),title: Text('Library')),
+          backgroundColor: Colors.white,
+        ),
+        body: children[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.explore), label: 'Explore'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.subscriptions), label: 'Subscriptions'),
+            BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'Inbox'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.video_library), label: 'Library'),
           ],
           showSelectedLabels: true,
           showUnselectedLabels: true,
@@ -84,69 +117,87 @@ class _BaseState extends State<Base> {
           unselectedItemColor: Colors.grey,
           onTap: onTabTapped,
           currentIndex: _currentIndex,
-          ),
+        ),
       ),
     );
   }
-  void onTabTapped(int index){
+
+  void onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
     });
   }
 }
 
-class Data{
+class Data {
   String imgLoc;
   String title;
   String channelLogo;
   String channelName;
   String videoViews;
   String uploadedago;
-  Data({this.channelLogo,this.channelName,this.imgLoc,this.title,this.uploadedago,this.videoViews});
+  Data(
+      {this.channelLogo,
+      this.channelName,
+      this.imgLoc,
+      this.title,
+      this.uploadedago,
+      this.videoViews});
 }
 
 class TileElement extends StatelessWidget {
   final data = Data(
-    title: 'Sample Title',
-    imgLoc: 'assets/images/SampleThumbnail.png',
-    channelLogo: 'assets/icons/channel.png',
-    channelName: 'Sample Name',
-    videoViews: '10k Views',
-    uploadedago: '1 month ago'
-  );
+      title: 'Sample Title',
+      imgLoc: 'assets/images/SampleThumbnail.png',
+      channelLogo: 'assets/icons/channel.png',
+      channelName: 'Sample Name',
+      videoViews: '10k Views',
+      uploadedago: '1 month ago');
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){Navigator.push(context, new MaterialPageRoute(builder: (context) => new VideoPlayerScreen()));},
+      onTap: () {
+        Navigator.push(
+            context,
+            new MaterialPageRoute(
+                builder: (context) => new VideoPlayerScreen()));
+      },
       child: Container(
-        padding: EdgeInsets.only(bottom: 50, top: 0),
-        child: Column(
-          children:<Widget>[
+          padding: EdgeInsets.only(bottom: 50, top: 0),
+          child: Column(children: <Widget>[
             Image.asset(data.imgLoc),
-            Row(children:<Widget>[
+            Row(children: <Widget>[
               Image.asset(data.channelLogo, width: 40, height: 30),
-              SizedBox(width:50),
+              SizedBox(width: 50),
               Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:<Widget>[
-                  Text(data.title, style: TextStyle(fontWeight: FontWeight.w500),),
-                  SizedBox(height: 20),
-                  Row(children:<Widget>[
-                      Text(data.channelName, style: TextStyle(color: Colors.grey),),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      data.title,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 20),
+                    Row(children: <Widget>[
+                      Text(
+                        data.channelName,
+                        style: TextStyle(color: Colors.grey),
+                      ),
                       Text('.'),
-                      Text(data.videoViews, style: TextStyle(color: Colors.grey),),
+                      Text(
+                        data.videoViews,
+                        style: TextStyle(color: Colors.grey),
+                      ),
                       Text('.'),
-                      Text(data.uploadedago, style: TextStyle(color: Colors.grey),)
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly
-                  )
-              ]),
+                      Text(
+                        data.uploadedago,
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ], mainAxisAlignment: MainAxisAlignment.spaceEvenly)
+                  ]),
               Spacer(),
               Icon(Icons.more_vert)
             ])
-          ]
-        )
-      ),
+          ])),
     );
   }
 }
